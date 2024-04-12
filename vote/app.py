215ecf657652f726493b028d3a46c92a0a7f6f58
Brian Morel <brianmorel99@gmail.com>
2024-04-12 14:45:10 +0000

    Initial modifications

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,9 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Aresenal")
+option_b = os.getenv('OPTION_B', "Liverpool")
+option_c = os.getenv('OPTION_C', "Manchester City")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -40,6 +41,7 @@ def hello():
         'index.html',
         option_a=option_a,
         option_b=option_b,
+        option_c=option_c
         hostname=hostname,
         vote=vote,
     ))
