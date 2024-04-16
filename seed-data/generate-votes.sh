6ef52e3137241c51cd04449ef6fc7f0fbc3c2f0d
Brian Morel <brianmorel99@gmail.com>
2024-04-16 19:12:36 +0000

    remove concurrent conections from seed data

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # create 3000 votes (2000 for option a, 1000 for option b)
-ab -n 3 -c 50 -p posta -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
-ab -n 4 -c 50 -p postb -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
-ab -n 2 -c 50 -p postc -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
+ab -n 3 -p posta -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
+ab -n 4 -p postb -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
+ab -n 2 -p postc -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
