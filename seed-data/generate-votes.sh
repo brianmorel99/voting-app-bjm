33141325ca7c015675323ccfd74f92595554121f
Brian Morel <brianmorel99@gmail.com>
2024-04-23 19:07:16 +0000

    Modified seed data script to ask user for IP addr.

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# create 3000 votes (2000 for option a, 1000 for option b)
-ab -n 3 -p posta -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
-ab -n 4 -p postb -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
-ab -n 2 -p postc -T "application/x-www-form-urlencoded" http://192.168.49.2:31000/
+echo Please enter the External IP of the Vote Pod:
+read addr
+echo IP Address $addr Entered.
+
+ab -n 3 -p posta -T "application/x-www-form-urlencoded" http://$addr:31000/
+ab -n 4 -p postb -T "application/x-www-form-urlencoded" http://$addr:31000/
+ab -n 2 -p postc -T "application/x-www-form-urlencoded" http://$addr:31000/
